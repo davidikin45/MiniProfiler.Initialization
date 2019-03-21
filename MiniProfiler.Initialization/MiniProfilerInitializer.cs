ef63a264707b94cd3cbcaa598de94298b0f68733
David Ikin <davidikin45@gmail.com>
2019-03-21 17:17:46 +0000

    Initial Commit

---
+++ MiniProfiler.Initialization/MiniProfilerInitializer.cs
@@ -0,0 +1,232 @@
+﻿using Database.Initialization;
+using Microsoft.Data.Sqlite;
+using StackExchange.Profiling.Storage;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MiniProfiler.Initialization
+{
+    public static class MiniProfilerInitializer
+    {
+        public static List<(string Schema, string Table)> TableNames => new List<(string Schema, string Table)>() {
+            ("dbo", "MiniProfilerClientTimings"),
+            ("dbo", "MiniProfilers"),
+            ("dbo", "MiniProfilerTimings")
+        };
+
+        public static async Task EnsureTablesDeletedAsync(string connectionString, CancellationToken cancellationToken = default)
+        {
+            var commands = new List<String>();
+            if (string.IsNullOrEmpty(connectionString))
+            {
+
+            }
+            else if (ConnectionStringHelper.IsSQLite(connectionString))
+            {
+                bool dbExists = await DbInitializer.ExistsAsync(connectionString, cancellationToken).ConfigureAwait(false);
+
+                if (dbExists)
+                {
+                    var persistedTables = await DbInitializer.TableNamesAsync(connectionString, cancellationToken).ConfigureAwait(false);
+
+                    using (var conn = new SqliteConnection(connectionString))
+                    {
+                        await conn.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+                        using (SqliteTransaction transaction = conn.BeginTransaction())
+                        {
+                            var deleteTables = TableNames.Where(x => persistedTables.Contains(x.Table));
+
+                            //Drop tables
+                            foreach (var tableName in deleteTables)
+                            {
+                                foreach (var t in deleteTables)
+                                {
+                                    try
+                                    {
+                                        var commandSql = $"DROP TABLE [{t.Schema}.{t.Table}];";
+                                        using (var command = new SqliteCommand(commandSql, conn, transaction))
+                                        {
+                                            await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+                                        }
+
+                                        commands.Add(commandSql);
+                                    }
+                                    catch
+                                    {
+
+                                    }
+                                }
+                            }
+
+                            transaction.Rollback();
+                        }
+
+                        using (SqliteTransaction transaction = conn.BeginTransaction())
+                        {
+                            foreach (var commandSql in commands)
+                            {
+                                using (var command = new SqliteCommand(commandSql, conn, transaction))
+                                {
+                                    await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+                                }
+                            }
+
+                            transaction.Commit();
+                        }
+                    }
+                }
+                else
+                {
+                    await DbInitializer.EnsureDestroyedAsync(connectionString, cancellationToken).ConfigureAwait(false);
+                }
+            }
+            else
+            {
+                bool dbExists = await DbInitializer.ExistsAsync(connectionString, cancellationToken);
+
+                if (dbExists)
+                {
+                    var persistedTables = await DbInitializer.TableNamesAsync(connectionString, cancellationToken).ConfigureAwait(false);
+
+                    using (var conn = new SqlConnection(connectionString))
+                    {
+                        await conn.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+                        using (SqlTransaction transaction = conn.BeginTransaction())
+                        {
+                            var deleteTables = TableNames.Where(x => persistedTables.Contains(x.Table));
+
+                            //Drop tables
+                            foreach (var tableName in deleteTables)
+                            {
+                                foreach (var t in deleteTables)
+                                {
+                                    try
+                                    {
+                                        var commandSql = $"DROP TABLE [{t.Schema}].[{t.Table}]";
+                                        using (var command = new SqlCommand(commandSql, conn, transaction))
+                                        {
+                                            await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+                                        }
+
+                                        commands.Add(commandSql);
+                                    }
+                                    catch
+                                    {
+
+                                    }
+                                }
+                            }
+
+                            transaction.Rollback();
+                        }
+
+                        using (SqlTransaction transaction = conn.BeginTransaction())
+                        {
+                            foreach (var commandSql in commands)
+                            {
+                                using (var command = new SqlCommand(commandSql, conn, transaction))
+                                {
+                                    await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+                                }
+                            }
+
+                            transaction.Commit();
+                        }
+                    }
+                }
+                else
+                {
+                    await DbInitializer.EnsureDestroyedAsync(connectionString, cancellationToken).ConfigureAwait(false);
+                }
+            }
+        }
+
+        public static Task<bool> EnsureDbCreatedAsync(string connectionString, CancellationToken cancellationToken = default)
+        {
+            return DbInitializer.EnsureCreatedAsync(connectionString, cancellationToken);
+        }
+
+        public static async Task<bool> EnsureDbAndTablesCreatedAsync(string connectionString, CancellationToken cancellationToken = default)
+        {
+            await EnsureDbCreatedAsync(connectionString, cancellationToken).ConfigureAwait(false);
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                return false;
+            }
+            else if (ConnectionStringHelper.IsSQLite(connectionString))
+            {
+                var persistedTables = await DbInitializer.TableNamesAsync(connectionString, cancellationToken).ConfigureAwait(false);
+
+                var storage = new SqliteStorage("");
+                var sqlScripts = storage.TableCreationScripts;
+                bool created = false;
+
+                using (var conn = new SqliteConnection(connectionString))
+                {
+                    await conn.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+                    using (SqliteTransaction transaction = conn.BeginTransaction())
+                    {
+                        foreach (var commandSql in sqlScripts.Where(sqlScript => !persistedTables.Any(table => sqlScript.Contains(table))))
+                        {
+                            using (var command = new SqliteCommand(commandSql, conn, transaction))
+                            {
+                                await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+                            }
+                            created = true;
+                        }
+
+                        transaction.Commit();
+                    }
+                }
+
+                return created;
+            }
+            else
+            {
+                var storage = new SqlServerStorage("");
+                var sqlScripts = storage.TableCreationScripts;
+                bool created = false;
+
+                //Initialize Schema
+                using (var conn = new SqlConnection(connectionString))
+                {
+                    await conn.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+                    using (SqlTransaction transaction = conn.BeginTransaction())
+                    {
+                        foreach (var commandSql in sqlScripts)
+                        {
+                            try
+                            {
+                                using (var command = new SqlCommand(commandSql, conn, transaction))
+                                {
+                                    await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+                                }
+                                created = true;
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+
+                        transaction.Commit();
+                    }
+                }
+                return created;
+            }
+        }
+
+        public static Task<bool> EnsureDbDestroyedAsync(string connectionString, CancellationToken cancellationToken = default)
+        {
+            return DbInitializer.EnsureDestroyedAsync(connectionString, cancellationToken);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,114 @@
+# MiniProfiler Initialization for SQL Server and SQLite
+
+* [MiniProfiler ASP.NET Core](https://miniprofiler.com/dotnet/AspDotNetCore)
+* I have created the following extension methods which aims to allow MiniProfiler to be initialized & destroyed independently in a similar manner to how EF Core initialization works. A key addition is the ability to create a new database if it doesn't already exist.
+* Supports SQL Server and SQLite connection strings.
+
+## Nuget packages
+* MiniProfiler.AspNetCore.Mvc
+* MiniProfiler.EntityFrameworkCore
+* MiniProfiler.Providers.SqlServer
+* MiniProfiler.Providers.Sqlite
+
+## Usage
+* await MiniProfilerInitializer.EnsureTablesDeletedAsync(connectionString) = Ensures only tables related to MiniProfiler are deleted.
+* await MiniProfilerInitializer.EnsureDbCreatedAsync(connectionString) = Ensures MiniProfiler physical database is created in preparation for MiniProfiler to create schema.
+* await MiniProfilerInitializer.EnsureDbAndTablesCreatedAsync(connectionString) = Ensures MiniProfiler physical database is created and tables created.
+* await MiniProfilerInitializer.EnsureDbDestroyedAsync(connectionString) = Deletes physical database.
+* Ensure Language Version is set to 'C# latest minor version (latest) to allow async Main.
+* Ensure Main method is async.
+```
+ public class Program
+{
+	public static async Task Main(string[] args)
+	{
+		
+	}
+}
+```
+
+## Development and Integration Environment Example
+```
+var sqlServerConnectionString = "Server=(localdb)\\mssqllocaldb;Database=MiniProfilerDatabase;Trusted_Connection=True;MultipleActiveResultSets=true;";
+await MiniProfilerInitializer.EnsureTablesDeleted(sqlServerConnectionString);
+await MiniProfilerInitializer.EnsureDbAndTablesCreatedAsync(sqlServerConnectionString);
+
+var sqliteConnectionString = "Data Source=MiniProfiler.db;";
+await MiniProfilerInitializer.EnsureTablesDeletedAsync(sqliteConnectionString);
+await MiniProfilerInitializer.EnsureDbAndTablesCreatedAsync(sqliteConnectionString);
+```
+
+## Staging and Production Environment Example
+```
+var sqlServerConnectionString = "Server=(localdb)\\mssqllocaldb;Database=MiniProfilerDatabase;Trusted_Connection=True;MultipleActiveResultSets=true;";
+await MiniProfilerInitializer.EnsureDbAndTablesCreatedAsync(sqlServerConnectionString);
+
+var sqliteConnectionString = "Data Source=MiniProfiler.db;";
+await MiniProfilerInitializer.EnsureDbAndTablesCreatedAsync(sqliteConnectionString);
+```
+
+## Example
+```
+public static async Task Main(string[] args)
+{
+     var host = CreateWebHostBuilder(args).Build();
+
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var configuration = services.GetRequiredService<IConfiguration>();
+			var connectionString = configuration.GetConnectionString("DefaultConnection");
+            await MiniProfilerInitializer.EnsureTablesDeletedAsync(connectionString);
+			await MiniProfilerInitializer.EnsureDbAndTablesCreatedAsync(connectionString);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred while initializing MiniProfiler db.");
+        }
+    }
+
+    host.Run();
+}
+
+public class Startup
+{
+     public IConfiguration Configuration { get; }
+
+	 public Startup(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
+	
+	public virtual void ConfigureServices(IServiceCollection services)
+	{
+		  services.AddMiniProfiler(options => {
+                options.PopupRenderPosition = StackExchange.Profiling.RenderPosition.BottomLeft;
+                options.PopupStartHidden = true; //ALT + P to display
+                options.PopupShowTimeWithChildren = true;
+                options.ResultsAuthorize = (request) => true;
+                options.UserIdProvider = (request) => request.HttpContext.User.Identity.Name;
+                //options.Storage = new SqlServerStorage();
+            }).AddEntityFramework();
+	}
+
+	public void Configure(IApplicationBuilder app)
+	{
+		// ...existing configuration...
+		app.UseMiniProfiler();
+
+		// The call to app.UseMiniProfiler must come before the call to app.UseMvc
+		app.UseMvc(routes =>
+		{
+			// ...
+		});
+			
+	}
+}
+```
+
+## See Also
+* [Database Initialization](https://github.com/davidikin45/Database.Initialization)
+* [EntityFrameworkCore Initialization](https://github.com/davidikin45/EntityFrameworkCore.Initialization)
